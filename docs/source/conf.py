2ed968a8c9f7f18a0085f91d3309b776b8932b70
Haihui Zhu <71397044+Haihui-Zhu@users.noreply.github.com>
2024-07-10 23:48:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'spartan on cloud'
 copyright = '2024, Haihui Zhu & ACAG'
 author = 'Haihui Zhu'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
